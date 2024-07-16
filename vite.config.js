f3a77f0d74ecd32f0fc167648f34e6079df7c288
Deepraj <deepraajdas@gmail.com>
2024-07-16 13:08:43 +0530

    update - added base in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://maniacone.github.io/Portfolio/",
   css: {
     modules: {
       localsConvention: "camelCase",
